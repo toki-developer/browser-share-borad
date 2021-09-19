b59c7ec0ac57ff0edb5a4c864a54ea1977e7d2bb
Toki <55911995+tokio-k@users.noreply.github.com>
2021-09-19 09:35:52 +0900

    Create Layout File

--- src/pages/brainstorming/[id].page.tsx
+++ src/pages/brainstorming/[id].page.tsx
@@ -1,13 +1,21 @@
 import { gql } from "@apollo/client";
 import { useRouter } from "next/dist/client/router";
+import { useBranistorming_GetBoradQuery } from "src/apollo/graphql";
+import { Layout } from "src/layout";
 
 const BrainstormingPage = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { data, error } = useBranistorming_GetBoradQuery({
+    variables: { id: (router.query.id as string) ?? "" },
+  });
+  if (error) return <div>error</div>;
+  if (!data) {
+    return <div>loading...</div>;
+  }
   return (
-    <div>
-      <h1 className="bg-red-400 text-black">{id}</h1>
-    </div>
+    <Layout>
+      <h1 className="bg-red-400 text-black">{data.borad_by_pk?.id}</h1>
+    </Layout>
   );
 };
 
