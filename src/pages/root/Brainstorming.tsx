6c2cf07c0889d261936ecc4a5af7d9047e1ab42d
Toki <55911995+tokio-k@users.noreply.github.com>
2021-09-21 10:21:59 +0900

    Add TOP title shadow

--- src/pages/root/Brainstorming.tsx
+++ src/pages/root/Brainstorming.tsx
@@ -20,15 +20,32 @@ export const Brainstorming: VFC<Props> = (props) => {
   };
   return (
     <div>
-      <div className="bg-blue-600 h-48 flex items-center justify-around ">
-        <h2 className="text-white text-3xl">
-          <span className="text-5xl">ブレインストーミング</span>を始める
+      <div className="bg-blue-600 h-96 flex items-center justify-around ">
+        <h2 className="text-white text-3xl font-bold">
+          <span className="text-5xl text-shadow-black-5">
+            ブレインストーミング
+          </span>
+          を始める
         </h2>
         <button
-          className="py-1 px-10 rounded-full text-2xl font-bold bg-white "
+          className="py-1 px-10 rounded-full text-2xl shadow-xl font-bold bg-white flex items-center hover:shadow-none hover:text-gray-600"
           onClick={handleClick}
         >
-          start
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-1 inline-block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+          START
         </button>
       </div>
       <Usage />
